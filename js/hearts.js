96f6ba0629ad44ed129108748eccbcb5c95ffa4d
jaredmpeterson <jared@peteyinc.com>
2016-06-01 13:02:11 -0600

    updates

--- js/hearts.js
+++
@@ -1,5 +0,0 @@
-angular.module('hearts').controller('dealer', function($scope) {
-
-  $scope.name = "Hearts";
-
-});
---
+++ js/mainCtrl.js
@@ -0,0 +1,5 @@
+angular.module('hearts').controller('mainCtrl', function($scope) {
+
+  $scope.name = "Hearts";
+
+});
